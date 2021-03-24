14ea4fd7d43f58c04b9f033b092733cb29eaeb92
helloAakash <asissunuwar78@gmail.com>
2021-03-24 15:55:39 +0545

    updated

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,9 +4,11 @@ import { Link } from "react-router-dom";
 function NavBar() {
   return (
     <>
-      <div className="container mt-3">
-        <div className="row">
+      <div className="container-fluid">
+        <div className="row align-items-center" style={{backgroundColor:"#131921"}}>
           <div className="col-md-8">
+
+
             <form className="d-flex">
               <input
                 className="form-control me-2"
@@ -14,7 +16,7 @@ function NavBar() {
                 placeholder="Search"
                 aria-label="Search"
               />
-              <button className="btn btn-outline-success" type="submit">
+              <button className="btn btn-outline-success text-white border-white" type="submit">
                 Search
               </button>
             </form>
@@ -22,24 +24,25 @@ function NavBar() {
         
 
           <div className="col-md-4">
-            <ul className="d-flex">
+            <ul className="d-flex align-items-center">
               <li className="list-unstyled">
-                <Link className="text-decoration-none p-3 text-dark" to="/Signup">
+                <Link className="text-decoration-none p-3 text-light" to="/Signup"><i class="bi bi-person-plus-fill p-2" style={{color:"orange",fontSize:'25px'}}></i>
                   Signup
                 </Link>
               </li>
               <li className="list-unstyled">
-                <Link className="text-decoration-none p-3 text-dark" to="/Signin">
+                <Link className="text-decoration-none p-3 text-light" to="/Signin"><i class="bi bi-box-arrow-in-right p-2"style={{color:"orange",fontSize:'25px'}}></i>
                   Signin
                 </Link>
               </li>
+              <li className="list-unstyled"><Link className="text-decoration-none p-2"><i class="bi bi-cart-fill" style={{color:'orange',fontSize:'30px'}}></i><sup style={{fontSize:'15px',backgroundColor:'red',borderRadius:'500px',padding:'3px',color:'white'}}>5</sup></Link></li>
             </ul>
           </div>
         </div>
       </div>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg navbar-light " style={{backgroundColor:'#232f32'}}>
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">
+          <Link className="navbar-brand text-white" to="/">
             E-Commerce
           </Link>
           <button
@@ -56,18 +59,18 @@ function NavBar() {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">
+                <Link className="nav-link active text-white" aria-current="page" to="/">
                   Home
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/offer">
+                <Link className="nav-link text-white" to="/offer">
                   Offer
                 </Link>
               </li>
               <li className="nav-item dropdown">
                 <Link
-                  className="nav-link dropdown-toggle"
+                  className="nav-link dropdown-toggle text-white"
                   to="#"
                   id="navbarDropdown"
                   role="button"
@@ -78,12 +81,12 @@ function NavBar() {
                 </Link>
                 <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
                   <li>
-                    <Link className="dropdown-item" to="#">
+                    <Link className="dropdown-item text-white" to="#">
                       Action
                     </Link>
                   </li>
                   <li>
-                    <Link className="dropdown-item" to="#">
+                    <Link className="dropdown-item text-white" to="#">
                       Another action
                     </Link>
                   </li>
@@ -91,21 +94,11 @@ function NavBar() {
                     <hr className="dropdown-divider" />
                   </li>
                   <li>
-                    <Link className="dropdown-item" to="#">
+                    <Link className="dropdown-item text-white" to="#">
                       Something else here
                     </Link>
                   </li>
                 </ul>
-              </li>
-              <li className="nav-item">
-                <Link
-                  className="nav-link disabled"
-                  to="#"
-                  tabindex="-1"
-                  aria-disabled="true"
-                >
-                  Disabled
-                </Link>
               </li>
             </ul>
           </div>
